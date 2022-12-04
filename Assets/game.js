8e82e9afeb561e78e4ec273b3910b938d5a45bc2
emileebogdan <emileebogdan@gmail.com>
2022-12-04 14:37:39 -0700

    question functionality

--- Assets/game.js
+++ Assets/game.js
@@ -1,41 +1,82 @@
 var timer;
 var timerInterval = document.getElementById("timerInterval");
+var question = document.getElementById("question");
+var button = document.getElementsByClassName("choice-container");
+var score = document.getElementById("score");
 
 let questions = [
   {
     question: "What Does JS Stand For?",
     choices: ["Absolutely Nothing", "John Stamos", "Java Script", "Java Joe's"
     ],
-    answer: "Java Script"
+    answer: 2
   },
 
   {
     question: "What Number is the First Object in an Array?",
     choices: ["612", "1", "I don't know, man.", "0"],
-    answer: "0"
+    answer: 3
   },
 
   {
     question: "What do you link in your HTML to make your JS work?",
     choices: ["Your Script File", "Your CSS File", "Nothing, it just Works", "Dang it, that's why it's not working"],
-    answer: "Your Script File",
+    answer: 0,
   }
 ]
+var currentQuestion = 0;
 
-var clockTick = function(){
-  var seconds = +timerInterval.textContent;
-  timerInterval.textContent = seconds - 1;
-  if (seconds <= 1) {
+var clockTick = function (decrement = 1) {
+  var seconds = Math.max(0,+timerInterval.textContent - decrement);
+  timerInterval.textContent = seconds;
+  if (seconds <= 0) {
+    endQuiz();
     clearInterval(timer);
-  } 
+  }
 };
-var getNewQuestion = function(){};
+var getNewQuestion = function () {
+  if (currentQuestion >= questions.length) {
+    return endQuiz();
+  }
+  const updateQuestion = questions[currentQuestion];
+  question.textContent = updateQuestion.question;
+  updateQuestion.choices.forEach((option, index) => {
+    var thisButton = button[index];
+    var choiceText = thisButton.getElementsByClassName("choice-text")[0];
+    choiceText.textContent = option;
+  })
+};
 
-startGame = () => {
+var answerQuestion = function (event) {
+  // verify if it's right or wrong
+  const btn = event.currentTarget;
+  const currQues = questions[currentQuestion];
+    // if wrong remove time
+  const isCorrect = currQues.answer == btn.dataset.number;
+  if (!isCorrect){
+    clockTick(2)
+  }
+  // if right give points
+  else {
+    score.textContent = +score.textContent + 10
+  }
+  // go to next question
+  currentQuestion++;
+  getNewQuestion();
+
+};
+
+var endQuiz = function() {
+  
+};
+
+var startGame = () => {
   timer = setInterval(clockTick, 1000)
   getNewQuestion()
-}
-
+};
 
 startGame();
 
+for (const btn of button) {
+  btn.addEventListener("click", answerQuestion)
+};
